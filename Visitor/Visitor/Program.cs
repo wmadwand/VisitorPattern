5e4414cf8da322388ce6d6ff4e82803a51002fa4
wmadwand <wmadwand@gmail.com>
2018-10-19 09:15:27 +0300

    Example01

---
+++ Visitor/Visitor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+	class Program1
+	{
+		static void Main1(string[] args)
+		{
+		}
+	}
+}
